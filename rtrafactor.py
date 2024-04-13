184a3fd14903aa96b3982d72a3251e8acec97d27
Udit Akhouri <156824020+UditAkhourii@users.noreply.github.com>
2024-04-13 12:15:05 +0530

    Update rtrafactor.py

--- rtrafactor.py
+++ rtrafactor.py
@@ -44,12 +44,3 @@ query = "Who is Udit Akhouri?"
 one_shot_answer = connector.compare_answers(query)
 print(one_shot_answer)
 
-
-Currently, we are in the midst of a race of who can scrape the most of the interent and put it on their servers so their model can answer it. This requires a lot of computation and makes it impossible for solo developers and small teams to make a super efficient model.
-
-RTRA architecture challenges this convection thought process. We belive that the interent is available for free and anyone can access it. That's why, you don't need to rely on huge computational resources (unline what Jansen Huang or Sam Altman says).
-
-We are not on a war with Open AI, Nvidia or Google. However, we are just democratizing the resources so even a 1st year undergrad student can build models as efficient as GPT4 right from his Google Colab notebook.
-
-We are currently testing our RTRA on low-parameter models and have successfully increased efficiency of Google's Gemma 7B to best Gemini 1.0 in multiple precision benchmarks. We are planning work specifically on models with <1 billion parameters to achieve the same efficiency of atleast a 7 billion parameter model with the help of RTRA architecture.
-"""
